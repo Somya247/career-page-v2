d56511b25848e53f9487d9ea3527b105246083de
Somya247 <96365969+Somya247@users.noreply.github.com>
2023-05-29 12:22:54 +0000

    added connection to db

--- database.py
+++ database.py
@@ -7,7 +7,6 @@ from sqlalchemy import create_engine, text
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 
-
 engine = create_engine(db_connection_string, 
                       connect_args={
                         "ssl":{
